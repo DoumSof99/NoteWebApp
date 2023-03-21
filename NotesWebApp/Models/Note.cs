01df81afaa98b6b33f32f7c64a767e7b84646479
DoumSof99 <sofiadmn99@gmail.com>
2023-03-21 20:29:10 +0200

    Added a Model, migrated it, the controller and add it to menu tabs

---
+++ NotesWebApp/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesWebApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteInfo{ get; set; }
+
+        public Note()
+        {
+
+        }
+    }
+}
